f8fa3fd62034f78a2584ccfdbd9dcc25b3b46878
Nicholas St. Germain <nick@cajun.pro>
2020-11-05 00:46:21 -0600

    allow env vars

--- images/conreq/README.md
+++ images/conreq/README.md
@@ -2,3 +2,13 @@
 
 ## **FOR DEMO PURPOSES ONLY**
 
+## Environment Variables
+All environment variables can override the credentials.json file
+
+| variable | key |
+| :----: | --- |
+| TMDB_KEY | tmdb_key |
+| SONARR_URL | sonarr_url |
+| SONARR_KEY | sonarr_key |
+| RADARR_URL | radarr_url |
+| RADARR_KEY | radarr_key |
--- images/conreq/entrypoint.sh
+++ images/conreq/entrypoint.sh
@@ -5,6 +5,7 @@ if [ ! -f "/config/credentials.json" ]; then
     cp /app/credentials.json.example /config/credentials.json
 fi
 
+jq
 if [ ! -f "/config/db.sqlite3" ]; then
     echo INFO: Initial migration
     python3 /app/manage.py migrate
@@ -12,6 +13,35 @@ if [ ! -f "/config/db.sqlite3" ]; then
                            --username itscontained \
                            --email itscontained@email.com
 fi
+
+config=$(cat /config/credentials.json)
+
+if [ -n "$TMDB_KEY" ]; then
+    echo INFO: TMDB_KEY overridden by environment variable
+    config=$(echo "$config" | jq --arg v "$TMDB_KEY" '.tmdb_key=$v')
+fi
+
+if [ -n "$SONARR_URL" ]; then
+    echo INFO: SONARR_URL overridden by environment variable
+    config=$(echo "$config" | jq --arg v "$SONARR_URL" '.sonarr_url=$v')
+fi
+
+if [ -n "$SONARR_KEY" ]; then
+    echo INFO: SONARR_KEY overridden by environment variable
+    config=$(echo "$config" | jq --arg v "$SONARR_KEY" '.sonarr_key=$v')
+fi
+
+if [ -n "$RADARR_URL" ]; then
+    echo INFO: RADARR_URL overridden by environment variable
+    config=$(echo "$config" | jq --arg v "$RADARR_URL" '.radarr_url=$v')
+fi
+
+if [ -n "$RADARR_KEY" ]; then
+    echo INFO: RADARR_KEY overridden by environment variable
+    config=$(echo "$config" | jq --arg v "$RADARR_KEY" '.radarr_key=$v')
+fi
+
+echo "$config" > /config/credentials.json
 
 echo INFO: Checking for migrations
 python3 /app/manage.py makemigrations
